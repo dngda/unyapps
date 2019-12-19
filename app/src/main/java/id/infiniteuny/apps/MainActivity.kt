1eb8df6dd5cbb78abac7c7b57b2f275a6837bcf7
dngda <opowaekeno123@gmail.com>
2019-12-19 17:08:27 +0700

    error

--- app/src/main/java/id/infiniteuny/apps/MainActivity.kt
+++ app/src/main/java/id/infiniteuny/apps/MainActivity.kt
@@ -2,11 +2,45 @@ package id.infiniteuny.apps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bot_navigation.setOnNavigationItemSelectedListener(this)
+        loadFragment(HomeFragment())
     }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId){
+            R.id.nav_home -> {
+
+            }
+            R.id.nav_map -> {
+
+            }
+            R.id.nav_profile -> {
+
+            }
+        }
+        return true
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+
+        if (fragment != null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+            return true
+        }
+        return false
+    }
+
 }
